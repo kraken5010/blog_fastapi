4ed6b1a6012150e67f3f7bd07e449625a1a182c4
kraken5010 <solokharts@gmail.com>
2023-03-17 22:49:49 +0200

    First commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, String, Integer, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+
+from core.db import Base
+
+
+class Post(Base):
+    __tablename__ = "blog_post"
+
+    id = Column(Integer, primary_key=True, unique=True, index=True)
+    title = Column(String)
+    text = Column(String(length=350))
+    date = Column(DateTime)
+    user = Column(Integer, ForeignKey("user.id"))
+    user_id = relationship("User")
---
+++ core/db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from core.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
